6edb44d86f13d40a60462df26ad60ff5b6cbfd43
Asish Panigrahy <asish.panigrahy02@gmail.com>
2019-10-30 23:04:05 +0530

    added driver initialization part

--- src/main/java/com/qa/pages/DealsPage.java
+++ src/main/java/com/qa/pages/DealsPage.java
@@ -1,9 +1,14 @@
 package com.qa.pages;
 
+import org.openqa.selenium.WebDriver;
+
 public class DealsPage {
 	
-	public DealsPage(){
-		
+	WebDriver driver;
+	
+	public DealsPage(WebDriver driver){
+	
+		this.driver=driver;
 	}
 	
 	public void dealspage(){
